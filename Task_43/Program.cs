c42d5b1891459d12dbc21d8811e1460020363c0a
Dima Bykadorov <dimka.b@live.com>
2023-08-25 12:40:28 +0300

    Add Task 43

---
+++ Task_43/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Прямые заданы уравнениями: y = k1 * x + b1, y = k2 * x + b2");
+
+Console.Write("\nВведите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"\nТочка пересечения двух прямых: {String.Join("; ", GetCrossPoint(b1, k1, b2, k2))}");
+
+double [] GetCrossPoint (double b1, double k1, double b2, double k2){
+    double [] crossPoint = new double[2];
+    double x = -(b1 - b2) / (k1 - k2);
+    double y = k2 * x + b2;
+    crossPoint[0] = Math.Round(x, 1);
+    crossPoint[1] = Math.Round(y, 1);
+    return crossPoint;
+}
