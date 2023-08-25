a799114a42f159a0761161f2cd8a755f3f512350
Dima Bykadorov <dimka.b@live.com>
2023-08-25 12:30:55 +0300

    Add Task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите строку чисел: ");
+string numbers = Console.ReadLine();
+Console.WriteLine($"Количество чисел больше 0: {GetNumberOfPositives(numbers)}");
+
+int GetNumberOfPositives (string numbers){
+    int count = 0;
+    string[] array = numbers.Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+    foreach (string el in array){
+        if (int.TryParse(el, out int temp) && temp > 0) count++;
+    }
+    return count;
+}
